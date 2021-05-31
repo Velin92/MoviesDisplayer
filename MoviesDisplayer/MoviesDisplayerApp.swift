ed46073e24387c959b11f003dcc953d8dde59852
Mauro Romito <maurovelin@yahoo.com>
2021-05-31 22:23:30 +0100

    Initial Commit

---
+++ MoviesDisplayer/MoviesDisplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDisplayerApp.swift
+//  MoviesDisplayer
+//
+//  Created by Mauro Romito on 31/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesDisplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
